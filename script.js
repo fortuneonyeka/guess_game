0500eac923476b53805f397e263a546810ac7918
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-09-23 07:56:37 +0100

    add score and score counter

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const checkBtn = document.querySelector('.check')
 const input = document.querySelector('.guess')
 const message = document.querySelector('.message')
+let scoreContent = document.querySelector('.score')
+let highScoreContent = document.querySelector('.highscore')
+let score = 20
+let highScore = 0
 
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1
@@ -14,13 +18,20 @@ checkBtn.addEventListener('click', function() {
   }else if (guess === secretNumber) {
     message.textContent = 'Congratulations, you win🏆'
     message.style.color = "green";
+    highScore = highScore + 10
+    highScoreContent.textContent = highScore
   }
   else if (guess < secretNumber) {
-    message.textContent = 'Too Low 📉'
+    message.textContent = 'Too Low 📉!'
+    
+    score = score -1
+    scoreContent.textContent = score
   }
 
   else if (guess > secretNumber) {
-    message.textContent = 'Too High 📈'
+    message.textContent = 'Too High 📈!'
+    score = score - 1
+    scoreContent.textContent = score
   }
   
 });
